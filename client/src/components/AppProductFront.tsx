821d65ffc0baa59f77001d8ddde230dd6b6fbd1f
artndev <97910780+artndev@users.noreply.github.com>
2025-05-21 17:51:27 +0300

    .

--- client/src/components/AppProductFront.tsx
+++ client/src/components/AppProductFront.tsx
@@ -1,16 +1,15 @@
-import React, { useEffect, useState } from 'react'
+import { Button } from '@/components/ui/button'
 import {
   Card,
   CardContent,
   CardDescription,
+  CardFooter,
   CardHeader,
   CardTitle,
-  CardFooter,
 } from '@/components/ui/card'
+import { Circle, CircleCheck } from 'lucide-react'
+import React, { useEffect, useState } from 'react'
 import config from '../config.json'
-import { Button } from '@/components/ui/button'
-import { Circle, CircleCheck, Heart, Plus } from 'lucide-react'
-import { Separator } from '@/components/ui/separator'
 import '../styles/css/ProductFront.css'
 
 const AppProductFront: React.FC<IProductFrontProps> = ({
@@ -22,17 +21,28 @@ const AppProductFront: React.FC<IProductFrontProps> = ({
   sizes,
   image,
 }) => {
-  const [size, setSize] = useState<{ M: string; W: string } | undefined>(
-    undefined
-  )
+  const [variant, setVariant] = useState<
+    { key: number; size: { M: string; W: string }; color: string } | undefined
+  >(undefined)
   const [color, setColor] = useState<string | undefined>(undefined)
+  const colors: string[] = Object.keys(sizes)
 
   useEffect(() => {
-    const key = Object.keys(sizes)[0]
+    const key = 0
+    if (!colors.length) return
 
-    setColor(key)
-    setSize(sizes[key!]![0])
+    const keyColor = colors[key]
+    setColor(keyColor)
+    setVariant({
+      key: key,
+      size: sizes[keyColor!]![key]!,
+      color: keyColor!,
+    })
   }, [])
+
+  // useEffect(() => {
+  //   console.log(color, variant)
+  // }, [color, variant])
 
   return (
     <div className="product__front-subcontainer flex gap-[20px] w-[min(800px,_100%)]">
@@ -48,52 +58,64 @@ const AppProductFront: React.FC<IProductFrontProps> = ({
             <CardDescription>{description}</CardDescription>
           </CardHeader>
           <CardContent>{details}</CardContent>
-          <CardFooter className="flex flex-col gap-[10px]">
-            <div className="grid grid-cols-[repeat(2,_1fr)] gap-[5px] w-full">
-              {Object.keys(sizes).map((val, i) => {
-                return (
-                  <Button
-                    key={i}
-                    className="flex justify-between"
-                    variant={'outline'}
-                    onClick={() => {
-                      if (color === val) {
-                        return
-                      }
+          <CardFooter className="card__footer flex flex-col gap-[10px]">
+            {color && (
+              <div className="grid grid-cols-[repeat(2,_1fr)] gap-[5px] w-full">
+                {colors.map((val, i) => {
+                  return (
+                    <Button
+                      key={i}
+                      className="flex justify-between"
+                      variant={'outline'}
+                      onClick={() => {
+                        if (color === val) {
+                          return
+                        }
 
-                      setColor(val)
-                    }}
-                  >
-                    <span>{val.charAt(0).toUpperCase() + val.slice(1)}</span>
-                    {color !== val ? <Circle /> : <CircleCheck />}
-                  </Button>
-                )
-              })}
-            </div>
-            <div className="flex flex-col gap-[5px] w-full">
-              {color &&
-                sizes[color!]!.map((val, i) => {
+                        setColor(val)
+                      }}
+                    >
+                      <span>{val.charAt(0).toUpperCase() + val.slice(1)}</span>
+                      {color !== val ? <Circle /> : <CircleCheck />}
+                    </Button>
+                  )
+                })}
+              </div>
+            )}
+            {color && variant && (
+              <div className="flex flex-col gap-[5px] w-full">
+                {sizes[color]!.map((val, i) => {
+                  console.log(color, val, i, variant.key, variant.color)
                   return (
                     <Button
                       key={i}
                       className="flex justify-between w-full"
                       variant={'outline'}
                       onClick={() => {
-                        if (size === val) {
+                        if (variant.key === i && variant.color === color) {
                           return
                         }
 
-                        setSize(val)
+                        setVariant({
+                          key: i,
+                          size: val,
+                          color: color,
+                        })
                       }}
                     >
                       <span>
                         M {val.M} / W {val.W}
                       </span>
-                      {size !== val ? <Circle /> : <CircleCheck />}
+                      {i !== variant.key || color !== variant.color ? (
+                        <Circle />
+                      ) : (
+                        <CircleCheck />
+                      )}
                     </Button>
                   )
                 })}
-            </div>
+              </div>
+            )}
           </CardFooter>
         </Card>
       </div>
